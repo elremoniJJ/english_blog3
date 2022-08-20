510fad9a727aaf6208510e751fcbe7e2fc5e205b
JJ Chapman <jacc1027@gmail.com>
2022-08-20 13:00:04 +0200

    fixing admin priviledges - bug#2

--- app.py
+++ app.py
@@ -548,7 +548,18 @@ def edit_post(id):
 @login_required
 def delete_post(id):
 	post = Posts.query.get_or_404(id)
-	if post.poster.id == current_user.id or current_user.id == 1:
+	if post.poster.id == current_user.id:
+		try:
+			db.session.delete(post)
+			db.session.commit()
+			flash('Post has been deleted!')
+			return redirect(url_for('all_posts'))
+
+		except:
+			flash('Something went wrong :/ Please try again later')
+			return redirect(url_for('all_posts'))
+
+	elif current_user.id == 1:
 		try:
 			db.session.delete(post)
 			db.session.commit()
